89b5fd20a9206afbf1e26c40de76425aa72e728d
Brady Olsen <bradyo@uw.edu>
2011-09-08 13:46:23 -0700

    updated code to working branch

---
+++ application/controllers/UserController.php
@@ -0,0 +1,64 @@
+<?php
+
+class UserController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        $this->view->identity = Zend_Auth::getInstance()->getIdentity();
+    }
+
+    public function loginAction()
+    {
+        $destination = $this->getRequest()->getParam('destination', null);
+
+        $form = new Application_Form_Login();
+        if ($this->getRequest()->isPost()) {
+            if ($form->isValid($this->getRequest()->getParams())) {
+                $auth = Zend_Auth::getInstance();
+
+                $db = Zend_Registry::get('db');
+                $adapter = new Zend_Auth_Adapter_DbTable($db, 'user',
+                    'username', 'password', 'MD5(?)');
+                $adapter->setIdentity($form->getValue('username'));
+                $adapter->setCredential($form->getValue('password'));
+
+                $result = $auth->authenticate($adapter);
+                if ($result->isValid()) {
+                    $username = $form->getValue('username');
+                    $data = $adapter->getResultRowObject(null, array('password'));
+                    $auth->getStorage()->write($data);
+
+                    if ($destination !== null) {
+                        $this->_redirect($destination, array('prependBase'=>false));
+                    } else {
+                        $this->_redirect('account');
+                    }
+                } else {
+									  $this->view->message = 'Username and password incorrect.';
+								}
+            }
+        }
+        $this->view->form = $form;
+    }
+
+    public function logoutAction()
+    {
+        $destination = $this->getRequest()->getParam('destination', null);
+
+        Zend_Auth::getInstance()->clearIdentity();
+        
+        if ($destination !== null) {
+            $this->_redirect($destination, array('prependBase'=>false));
+        } else {
+            $this->_redirect('/');
+        }
+    }
+
+}
+
--- public/index.php
+++ public/index.php
@@ -1,8 +1,16 @@
 <?php
 
-require_once('global.php');
-require_once 'Zend/Application.php';
+define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+define('APPLICATION_ENV', 'development');
 
+set_include_path(implode(PATH_SEPARATOR, array(
+    realpath(APPLICATION_PATH . '/../library'),
+    realpath(APPLICATION_PATH . '/../library/ZendFramework-1.10.8/library'),
+    realpath(APPLICATION_PATH . '/../library/doctrine'),
+    get_include_path(),
+)));
+
+require_once 'Zend/Application.php';
 $application = new Zend_Application(
     APPLICATION_ENV,
     APPLICATION_PATH . '/configs/application.ini'
