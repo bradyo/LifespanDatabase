638c0f65c8793b9ca8639f2e7048599d09a3e58c
Brady Olsen <bradyo@uw.edu>
2011-09-08 01:24:43 -0700

    refactored

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,13 +2,83 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-    protected function _initDoctype()
-    {
-        $this->bootstrap('view');
-        $view = $this->getResource('view');
-        $view->doctype('XHTML1_STRICT');
+     protected function _initAutoloader() {
+        $loader = Zend_Loader_Autoloader::getInstance();
+        $loader->registerNamespace('Doctrine');
+        $loader->registerNamespace('NP');
+        $loader->registerNamespace('Application');
     }
 
+    protected function _initHtmlPurifier() {
+        require_once 'HTMLPurifier/Bootstrap.php';
+        spl_autoload_register(array('HTMLPurifier_Bootstrap', 'autoload'));
+    }
+
+    protected function _initLayout() {
+        Zend_Layout::startMvc();
+        $layout = Zend_Layout::getMvcInstance();
+        $layout->setLayoutPath(APPLICATION_PATH . '/layouts/scripts');
+        $layout->setLayout('layout');
+        $layout->startMvc();
+    }
+    
+    protected function _initFrontControllerOptions() {
+        $front = Zend_Controller_Front::getInstance();
+        $front->setControllerDirectory(APPLICATION_PATH . '/controllers');
+    }
+    
+    protected function _initView() {
+        $view = new Zend_View();
+        
+        $view->addHelperPath('Application/View/Helper', 'Application_View_Helper');
+        $view->addHelperPath('NP/View/Helper', 'NP_View_Helper');
+
+        $view->setEncoding('UTF-8');
+        $view->doctype('XHTML1_STRICT');
+
+        $view->headMeta()->appendHttpEquiv('Content-Type', 'text/html; charset=UTF-8');
+        $view->headMeta()->appendHttpEquiv('Content-Language', 'en-US');
+
+        $view->headScript()->appendFile('/library/jquery-1.4.4.min.js');
+        $view->headScript()->appendFile('/library/jquery.tools.min.js');
+
+        $view->headLink()->appendStylesheet('/css/global.css');
+        $view->headScript()->appendFile('/js/global.js');
+
+        $view->headLink()->appendStylesheet('/library/jquery-ui/css/custom/jquery-ui-1.8.7.custom.css');
+        $view->headScript()->appendFile('/library/jquery-ui/js/jquery-ui-1.8.7.custom.min.js');
+
+        $view->headScript()->appendFile('/library/fancybox/jquery.fancybox-1.3.2.pack.js');
+        $view->headLink()->appendStylesheet('/library/fancybox/jquery.fancybox-1.3.2.css', 'screen');
+
+        $view->headTitle('Sageweb');
+        $view->headTitle()->setSeparator(' - ');
+        
+        // add view helper
+        $viewRenderer = new Zend_Controller_Action_Helper_ViewRenderer();
+        $viewRenderer->setView($view);
+        Zend_Controller_Action_HelperBroker::addHelper($viewRenderer);
+    }
+
+    protected function _initPlugins() {
+        $this->bootstrap('frontController');
+        $front = Zend_Controller_Front::getInstance();
+        $front->registerPlugin(new Application_Plugin_Access());
+    }
+
+    protected function _initCache() {
+        $frontend = array(
+            'lifetime' => 7200,
+            'automatic_serialization' => true,
+        );
+        $backend = array(
+            'cache_dir' => DATA_PATH . '/cache/application'
+        );
+        $cache = Zend_Cache::factory('core', 'File', $frontend, $backend);
+        Zend_Registry::set('cache', $cache);
+    }
+
+   
     protected function _initDatabases()
     {
         $this->bootstrap('multidb');
@@ -47,29 +117,52 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         return $options;
     }
 
-    protected function _initSession()
-    {
-        $session = $this->getPluginResource('session');
-        $session->init();
+    protected function _initSession() {     
+        $this->bootstrap('databases');
+        $config = array(
+            'name'           => 'session',
+            'primary'        => 'id',
+            'modifiedColumn' => 'modified',
+            'dataColumn'     => 'data',
+            'lifetimeColumn' => 'lifetime'
+        );
+        $sessionHandler = new Zend_Session_SaveHandler_DbTable($config);
+        Zend_Session::setSaveHandler($sessionHandler);
         Zend_Session::start();
     }
-
-    protected function _initRoutes()
-    {
+    
+    protected function _initRoutes() {
         $this->bootstrap('frontController');
-
         $router = $this->frontController->getRouter();
+        $router->setChainNameSeparator('/');
+        
+        $configPath = APPLICATION_PATH . '/configs/routes.ini';
+        $config = new Zend_Config_Ini($configPath);
+        $router->addConfig($config, 'routes');
+    }
 
-        // gene routes
-        $route = new Zend_Controller_Router_Route(
-            'genes/*',
-            array(
-                'module'        => 'default',
-                'controller'    => 'gene',
-                'action'        => 'index',
-            )
-        );
-        $router->addRoute('catalog_category_product', $route);
+    protected function _initI18n() {
+        // set up timezone
+        date_default_timezone_set('America/Los_Angeles');
+
+        // set up locale
+        $locale = Zend_Locale::setDefault('en_US');
+        Zend_Registry::set('Zend_Locale', $locale);
+    }
+
+    protected function _initLucene() {
+        // allow numbers in searches (default is alpha only)
+        $analyzer = new Zend_Search_Lucene_Analysis_Analyzer_Common_Utf8Num_CaseInsensitive();
+        Zend_Search_Lucene_Analysis_Analyzer::setDefault($analyzer);
+
+        // create search index
+        $indexPath = DATA_PATH . '/lucene/index';
+        try {
+            $index = Zend_Search_Lucene::open($indexPath);
+        } catch (Zend_Search_Lucene_Exception $e) {
+            $index = Zend_Search_Lucene::create($indexPath);
+        }
+        Zend_Registry::set('searchIndex', $index);
     }
 }
 
---
+++ application/configs/schema/core.sql
@@ -0,0 +1,318 @@
+
+CREATE TABLE citation (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    year INT,
+    author VARCHAR(255),
+    title VARCHAR(255),
+    source VARCHAR(255),
+    pubmed_id INT,
+    INDEX (year),
+    INDEX (author),
+    INDEX (title),
+    INDEX (source),
+    INDEX (pubmed_id)
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
+
+CREATE TABLE species (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    guid CHAR(32) NOT NULL,
+    name VARCHAR(128) NOT NULL,
+    common_name VARCHAR(128),
+    ncbi_tax_id INT,    
+    INDEX (guid),
+    INDEX (common_name),
+    INDEX (name),
+    INDEX (ncbi_tax_id)
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
+
+CREATE TABLE species_synonym (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    species_id INT NOT NULL,
+    type VARCHAR(64),
+    synonym VARCHAR(128) NOT NULL,
+    FOREIGN KEY (species_id) REFERENCES species (id) ON DELETE CASCADE
+    INDEX (type),
+    INDEX (synonym)
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
+
+CREATE TABLE strain (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    guid CHAR(32) NOT NULL,
+    species_id INT,
+    name VARCHAR(128) NOT NULL,
+    common_name VARCHAR(128),
+    INDEX (guid),
+    FOREIGN KEY (species_id) REFERENCES species (id) ON DELETE SET NULL
+    INDEX (name),
+    INDEX (common_name)
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
+
+
+CREATE TABLE compound (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    guid CHAR(32) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
+    ncbi_compound_id INT,
+    INDEX (guid),
+    INDEX (ncbi_compound_id),
+    INDEX (name)
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
+
+CREATE TABLE compound_synonym (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    compound_id INT NOT NULL,
+    synonym VARCHAR(64) NOT NULL,
+    INDEX (compound_id),
+    INDEX (synonym),
+    FOREIGN KEY (compound_id) REFERENCES compound (id) ON DELETE CASCADE
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
+
+
+CREATE TABLE gene (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    guid CHAR(32) NOT NULL,
+    species_id INT,
+    species VARCHAR(64),
+    symbol VARCHAR(64),
+    locus_tag VARCHAR(64),
+    description VARCHAR(255),
+    ncbi_gene_id INT,
+    INDEX (guid),
+    FOREIGN KEY (species_id) REFERENCES species (id) ON DELETE SET NULL
+    INDEX (symbol),
+    INDEX (locus_tag),
+    INDEX (ncbi_gene_id)
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
+
+CREATE TABLE gene_synonym (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    gene_id INT NOT NULL,
+    synonym VARCHAR(64) NOT NULL,
+    INDEX (gene_id),
+    INDEX (synonym),
+    FOREIGN KEY (gene_id) REFERENCES gene (id) ON DELETE CASCADE
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
+
+CREATE TABLE gene_link (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    gene_id INT NOT NULL,
+    database_id VARCHAR(64) NOT NULL,
+    identifier VARCHAR(64) NOT NULL,
+    INDEX (gene_id),
+    INDEX (database_id),
+    INDEX (identifier),
+    FOREIGN KEY (gene_id) REFERENCES gene (id) ON DELETE CASCADE
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
+
+CREATE TABLE gene_go (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    gene_id INT NOT NULL,
+    go_id VARCHAR(64) NOT NULL,
+    evidence VARCHAR(64),
+    category VARCHAR(64),
+    term VARCHAR(255),
+    INDEX (gene_id),
+    INDEX (go_id),
+    INDEX (evidence),
+    INDEX category_index(category),
+    FOREIGN KEY (gene_id) REFERENCES gene (id) ON DELETE CASCADE
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
+
+CREATE TABLE gene_homolog (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    gene_id INT NOT NULL,
+    ncbi_gene_id INT,
+    homolog_gene_id INT,
+    homolog_ncbi_gene_id INT,
+    algorithm VARCHAR(64),
+    family VARCHAR(64),
+    species VARCHAR(64),
+    symbol VARCHAR(64),
+    protein_database VARCHAR(64),
+    protein_id VARCHAR(64),
+    INDEX (gene_id),
+    INDEX (ncbi_gene_id),
+    INDEX (homolog_gene_id),
+    INDEX (homolog_ncbi_gene_id),
+    INDEX (algorithm),
+    INDEX (family),
+    INDEX (species),
+    INDEX (symbol),
+    INDEX (protein_database),
+    INDEX (protein_id),
+    FOREIGN KEY (gene_id) REFERENCES gene (id) ON DELETE CASCADE,
+    FOREIGN KEY (homolog_gene_id) REFERENCES gene (id) ON DELETE SET NULL
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
+
+CREATE TABLE gene_interaction (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    gene_id INT NOT NULL,
+    name VARCHAR(64) NOT NULL,
+    source VARCHAR(128),
+    pubmed_id INT,
+    partner_gene_id INT,
+    INDEX (gene_id),
+    INDEX (name),
+    INDEX (source),
+    INDEX (pubmed_id),
+    INDEX (partner_gene_id),
+    FOREIGN KEY (gene_id) REFERENCES gene (id) ON DELETE CASCADE,
+    FOREIGN KEY (partner_gene_id) REFERENCES gene (id) ON DELETE SET NULL
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
+
+CREATE TABLE gene_substrate (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    gene_id INT NOT NULL,
+    name VARCHAR(64) NOT NULL,
+    ncbi_compound_id INT,
+    compound_id INT,
+    INDEX (gene_id),
+    INDEX (name),
+    INDEX (ncbi_compound_id),
+    INDEX (compound_id),
+    FOREIGN KEY (gene_id) REFERENCES gene (id) ON DELETE CASCADE,
+    FOREIGN KEY (compound_id) REFERENCES compound (id) ON DELETE SET NULL
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
+
+
+CREATE TABLE comment (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    parent_guid CHAR(32) NOT NULL,
+    status VARCHAR(64) NOT NULL,
+    authored_at DATETIME,
+    author_id INT,
+    review_status VARCHAR(64),
+    reviewed_at DATETIME,
+    reviewer_id INT,
+    author_name VARCHAR(255),
+    author_email VARCHAR(255),
+    body TEXT,
+    INDEX (parent_guid),
+    INDEX (status),
+    INDEX (review_status),
+    INDEX (authored_at),
+    FOREIGN KEY (author_id) REFERENCES `user` (id) ON DELETE SET NULL,
+    INDEX (reviewed_at),
+    FOREIGN KEY (reviewer_id) REFERENCES `user` (id) ON DELETE SET NULL
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
+
+
+
+
+CREATE TABLE observation (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    guid CHAR(32) NOT NULL,
+    public_id INTEGER NOT NULL,
+    version INTEGER,
+    status VARCHAR(64),
+    authored_at DATETIME,
+    author_id INT,
+    review_status VARCHAR(64),
+    reviewed_at DATETIME,
+    reviewer_id INT,
+    reviewer_comment TEXT,
+
+    created_at DATETIME,
+    updated_at DATETIME,
+    lifespan DOUBLE,
+    lifespan_base DOUBLE,
+    lifespan_units VARCHAR(64),
+    lifespan_change DOUBLE,
+    lifespan_effect VARCHAR(64),
+    lifespan_measure VARCHAR(64),
+    taxonomy_id INT,
+    species VARCHAR(64),
+    strain VARCHAR(64),
+    cell_type VARCHAR(64),
+    mating_type VARCHAR(64),
+    temperature DECIMAL(5,2),
+    citation_pubmed_id INT,
+    citation_year INT,
+    citation_author VARCHAR(255),
+    citation_title VARCHAR (255),
+    citation_source VARCHAR (255),
+    body TEXT,
+
+    INDEX (public_id, version),
+    INDEX (public_id),
+    INDEX (version),
+    INDEX (status),
+    FOREIGN KEY (author_id) REFERENCES `user` (id) ON DELETE SET NULL,
+    INDEX (authored_at), 
+    INDEX (review_status),
+    INDEX (reviewed_at),
+    FOREIGN KEY (reviewer_id) REFERENCES `user` (id) ON DELETE SET NULL,
+    INDEX (created_at),
+    INDEX (updated_at), 
+    INDEX (lifespan),
+    INDEX (lifespan_base),
+    INDEX (lifespan_units),
+    INDEX (lifespan_change),
+    INDEX (lifespan_effect),
+    INDEX (lifespan_measure),
+    FOREIGN KEY (taxonomy_id) REFERENCES taxonomy (id) ON DELETE SET NULL,
+    INDEX (species),
+    INDEX (strain),
+    INDEX (cell_type),
+    INDEX (mating_type),
+    INDEX (temperature),
+    INDEX (citation_pubmed_id),
+    INDEX (citation_year),
+    INDEX (citation_author),
+    INDEX (citation_title),
+    INDEX (citation_source)
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
+
+CREATE TABLE observation_stats (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    observation_id INT NOT NULL,
+    gene_count INT NOT NULL DEFAULT(0),
+    compound_count INT NOT NULL DEFAULT (0),
+    environment_count INT NOT NULL DEFAULT (0),
+    FOREIGN KEY (observation_id) REFERENCES observation (id) ON DELETE CASCADE,
+    INDEX (gene_count),
+    INDEX (compound_count),
+    INDEX (environment_count)
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
+
+
+CREATE TABLE observation_gene (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    observation_id INT NOT NULL,
+    gene_id INT,
+    allele_type VARCHAR(64),
+    allele VARCHAR(64),
+    FOREIGN KEY (observation_id) REFERENCES observation (id) ON DELETE CASCADE,
+    FOREIGN KEY (gene_id) REFERENCES gene (id) ON DELETE SET NULL,    
+    INDEX (allele_type),
+    INDEX (allele)
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
+
+CREATE TABLE observation_compound (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    observation_id INT NOT NULL,
+    compound_id INT,
+    quantity VARCHAR(64),
+    FOREIGN KEY (observation_id) REFERENCES observation (id) ON DELETE CASCADE,
+    FOREIGN KEY (compound_id) REFERENCES compound (id) ON DELETE SET NULL,
+    INDEX (quantity)
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
+
+CREATE TABLE observation_environment (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    observation_id INT NOT NULL,
+    type VARCHAR(64) NOT NULL,
+    body TEXT NOT NULL,
+    FOREIGN KEY (observation_id) REFERENCES observation (id) ON DELETE CASCADE,
+    INDEX (type)
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
+
+
+CREATE TABLE featured_observation (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    observation_public_id INT NOT NULL,
+    position INT DEFAULT(0),
+    INDEX (observation_public_id),
+    INDEX (position)
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
