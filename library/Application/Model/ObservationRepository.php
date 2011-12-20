bebffff57eb6d3005b544cafc8ee49ba25d8a4a9
Brady Olsen <bradyo@uw.edu>
2011-12-20 15:40:51 -0800

    update

--- library/Application/Model/ObservationRepository.php
+++ library/Application/Model/ObservationRepository.php
@@ -6,5 +6,31 @@ use Doctrine\ORM\Entity\EntityRepository;
 
 class ObservationRepository extends EntityRepository 
 {
-    
+    public function getCurrent($criteria, $orderBy, $limit, $offset) {
+        
+        // todo: fetch id subset from fulltext search
+        $searchMatchIds = array();
+        
+        $dql = '
+            SELECT observation
+            FROM (
+                SELECT observationSubset
+                FROM (
+                        SELECT id, MAX(authoredAt) as versionDate 
+                        FROM Application\Model\Observation GROUP BY publicId
+                ) AS observationSubset
+                LEFT JOIN Application\Model\Observation o ON o.id = observationSubset.id
+            ) as observation
+            WHERE observation.status = :status 
+            AND observation.reviewStatus = :reviewStatus
+            ';
+        if (count($searchMatchIds) > 0) {
+            $dql .= ' AND observation.id IN (' . implode(', ', $searchMatchIds) . ')';
+        }
+        $query = $this->getEntityManager()->createQuery($dql);
+        $query->setParameter('status', Observation::STATUS_PUBLIC);
+        $query->setParameter('reviewStatus', Observation::REVIEW_STATUS_ACCEPTED);
+        
+        return $query->getResult();
+    }
 }
